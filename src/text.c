f9797f1be919c1c585457d61db3f0ce17a06ebbc
Eg0Ri4 <egor9101112@gmail.com>
2025-02-12 20:53:01 +0100

    text

---
+++ src/text.c
@@ -0,0 +1,8 @@
+#include "includs.h"
+
+void draw_info(int w, int m, int f, int s){
+    DrawText(TextFormat("Wave: %08i", w), 10, 20, 20, RED);
+    DrawText(TextFormat("Money: %08i$", m), 10, 40, 20, YELLOW);
+    DrawText(TextFormat("Food: %08i", f), 10, 60, 20, BROWN);
+    DrawText(TextFormat("Stone: %08i", s), 10, 80, 20,  GRAY);
+}
